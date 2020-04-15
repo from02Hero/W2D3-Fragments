7014e2acdaec0e056ad74ec3be06d337e039af9d
srgarcia <srgarcia@enhanceit.com>
2020-04-15 13:49:33 -0400

    Dynamically adding a fragment

--- app/src/main/java/com/example/w2d3_fragments/MainActivity.kt
+++ app/src/main/java/com/example/w2d3_fragments/MainActivity.kt
@@ -8,5 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1
+        if (savedInstanceState == null) {
+            // 2
+            supportFragmentManager
+                // 3
+                .beginTransaction()
+                // 4
+                .add(R.id.details, DogDetailsFragment.newInstance(), "dogDetails")
+                // 5
+                .commit()
+        }
     }
 }
